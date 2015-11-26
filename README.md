7cd1cd616c314666b315c8cc125cf9a27f17db08
Iván Sánchez Ortega <ivan@mazemap.no>
2015-11-26 13:26:13 +0100

    Converted gobble-sass into gobble-sass-all

--- README.md
+++ README.md
@@ -1,26 +1,45 @@
-# gobble-sass
+# gobble-sass-all
 
-Compile SASS files with gobble.
+Compile all SASS files with gobble.
 
 ## Installation
 
 First, you need to have gobble installed - see the [gobble readme](https://github.com/gobblejs/gobble) for details. Then,
 
 ```bash
-npm i -D gobble-sass
+npm i -D gobble-sass-all
 ```
 
 ## Usage
 
 ```js
 var gobble = require( 'gobble' );
-module.exports = gobble( 'src/styles' ).transform( 'sass', {
-  src: 'main.scss',
-  dest: 'main.css'
-});
+module.exports = gobble( 'src/styles' ).transform( 'sass-all' );
 ```
 
+That will transform all `*.sass` files in `src/styles` into `*.css` files.
+
+Optionally, specify a second argument to pass options to [node-sass](https://github.com/sass/node-sass), for example:
+
+```js
+var gobble = require( 'gobble' );
+module.exports = gobble( 'src/styles' ).transform( 'sass-all', {
+	indentedSyntax: true,
+	precision: 3,
+	sourceComments: true
+});
+```
 
 ## License
 
 MIT. Copyright 2015 Rich Harris
+
+---
+
+"THE BEER-WARE LICENSE":
+<ivan@sanchezortega.es> wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.
+
+---
+
--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-var path = require( 'path' );
-var sander = require( 'sander' );
 var _sass = require( 'node-sass' );
 
-module.exports = function sass ( inputdir, outputdir, options, callback ) {
-	if ( !options.src || !options.dest ) {
-		throw new Error( 'gobble-sass requires `options.src` and `options.dest` to be set' );
-	}
+module.exports = function sassAll(input, options) {
+	options.data = input;
+	return _sass.renderSync(options).css;
+};
 
-	options.file = path.join( inputdir, options.src );
-
-	if ( options.sourceMap !== false ) {
-		// by default, generate sourcemaps and include comments
-		options.outFile = path.join( outputdir, options.dest );
-		options.sourceMap = path.join( outputdir, options.dest + '.map' );
-		options.sourceMapRoot = path.dirname( options.outFile );
-		options.sourceMapContents = options.sourceMapContents !== false;
-	}
-
-	_sass.render( options, function ( err, result ) {
-		if ( err ) return callback( err );
-
-		var promises = [ sander.writeFile( outputdir, options.dest, result.css ) ];
-
-		if ( result.map ) {
-			promises.push( sander.writeFile( outputdir, options.dest + '.map', result.map ) );
-		}
-
-		sander.Promise.all( promises ).then( function () { callback(); }, callback );
-	});
+module.exports.defaults = {
+	accept: ".sass",
+	ext: ".css"
 };
