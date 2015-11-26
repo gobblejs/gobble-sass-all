3c630115263f5d7bf9fcffd74be33c155546b9e2
Iván Sánchez Ortega <ivan@mazemap.no>
2015-11-26 14:13:24 +0100

    Manage sourcemaps

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var _sass = require( 'node-sass' );
+var sass = require('node-sass');
+var fs = require('fs');
 
 module.exports = function sassAll(input, options) {
 	options.data = input;
-	return _sass.renderSync(options).css;
+	options.sourceMap = 'foobar.map';
+
+	var compiled = sass.renderSync(options);
+
+	var out = {
+		code: compiled.css.toString().replace('/*# sourceMappingURL=foobar.map */',''),
+		map: compiled.map.toString()
+	};
+
+	return out;
 };
 
 module.exports.defaults = {
